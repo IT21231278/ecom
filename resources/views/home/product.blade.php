9310983741dbf472c928c5f09ef91c9eeac38d4a
K.Rashmika Lankari Perera <rashmilankari19@gmail.com>
2023-09-26 17:22:39 +0530

    update product.blade

--- resources/views/home/product.blade.php
+++ resources/views/home/product.blade.php
@@ -6,6 +6,9 @@
                </h2>
             </div>
             <div class="row">
+
+
+            @foreach($product as $product)
                <div class="col-sm-6 col-md-4 col-lg-4">
                   <div class="box">
                      <div class="option_container">
@@ -19,19 +22,42 @@
                         </div>
                      </div>
                      <div class="img-box">
-                        <img src="home/images/p1.png" alt="">
+                        <img src="product/{{$product->image}}" alt="">
                      </div>
                      <div class="detail-box">
                         <h5>
-                           Men's Shirt
+                           {{$product->title}}                        
                         </h5>
-                        <h6>
-                           $75
+
+                        @if($product->discount_price!=null)
+
+                        <h6 style="color:red">
+                        Discount_Price
+                        <br>
+                        ${{$product->discount_price}}
                         </h6>
-                     </div>
+
+
+                        <h6 style="text-decoration:line-through;">
+                        Price
+                        <br>
+                        ${{$product->price}}
+                        </h6>
+
+                        @else
+
+                        <h6 style="color:green">
+                        Price
+                        <br>                       
+                        ${{$product->price}}
+                        </h6>
+
+                        @endif
+                       
+                  </div>
                   </div>
                </div>
-              
+              @endforeach
             
          </div>
       </section>
