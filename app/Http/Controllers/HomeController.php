79127862ae04bf4ce9fb4dc62b3320ddd566834c
K.Rashmika Lankari Perera <rashmilankari19@gmail.com>
2023-09-28 09:09:22 +0530

    modified

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,24 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
-use App\Models\Product;
 
 class HomeController extends Controller
 {
-    public function index()
-    {
-        $product=Product::all();
-        
-        return view('home.userpage',compact('product'));
-
-
-    }
-
-
-
-
-
-
     public function redirect(){
         $usertype=Auth::user()->userType;
         if($usertype=='1')
@@ -35,5 +20,10 @@ class HomeController extends Controller
         }   
     }
 
-    
+    public function index()
+    {
+        return view('home.userpage');
+
+
+    }
 }
